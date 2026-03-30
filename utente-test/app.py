296796954e06d5c77a87a88e87b9224e865c729e
gianluca.marini <gianluca.marini@top-network.it>
2026-03-30 15:22:18 +0200

    Rimozione lavoro Utente-Test

--- utente-test/app.py
+++
@@ -1,6 +0,0 @@
-import random
-from datetime import datetime
-
-print("Utente Test")
-print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-print(random.randint(1, 10))
--- utente-test/run.sh
+++
@@ -1,2 +0,0 @@
-sudo docker build -t  app_v:1 .
-docker run app_v:1 > text.txt
